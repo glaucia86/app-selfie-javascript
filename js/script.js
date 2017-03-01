d5ec8da6e487bef7d16e50b9c1cbb6f294aed2a6
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-03-01 16:55:27 -0300

    Desenvolvimento do script.js - app.

--- js/script.js
+++ js/script.js
@@ -20,8 +20,78 @@ navigator.getMedia = ( navigator.getUserMedia ||
 if(!navigator.getMedia) {
 	displayErrorMessage("Seu browser não tem suporte para o navigator.getUserMedia");
 } else {
+	// Solicita a câmera:
+	navigator.getMedia(
+		{
+			video: true
+		},
 
+		// Callback - Sucesso:
+		function(stream) {
+
+			// Criará um objeto URL para o stream de video e irá
+			// iniciar como src do nosso elemento video do HTML
+			video.src = window.URL.createObjectURL(stream);
+
+			// E depois irá dar um play no elemento video para mostrar o stream de video para o usuário:
+			video.play();
+			video.onplay = function() {
+				showVideo();
+			};
+		},
+
+		// Callback - Error:
+		function(err) {
+			displayErrorMessage("Ococrreu um erro ao acessar o stream de vídeo " + err.name, err);
+		}
+	);
 }
+
+// Navegadores de celulares não podem reproduzir video sem entrada do usuário.
+// Com isso, iremos utilizar um botão para iniciar a app manualmente:
+start_camera.addEventListener("click", function(e) {
+
+	e.preventDefault();
+
+	// Iniciar o video manualmente:
+	video.play();
+	showVideo();
+});
+
+take_photo_btn.addEventListener("click", function(e) {
+
+	e.preventDefault();
+
+	var snap = takeSnapshot();
+
+	// Mostrar imagem:
+	image.setAttribute('src', snap);
+	image.classList.add("visible");
+
+	// Ativar os botões: excluir e salvar:
+	delete_photo_btn.classList.remove("disabled");
+	download_photo_btn.classList.remove("disabled");
+
+	//Definir o atributo href do botão de download para a URL:
+	download_photo_btn.href = snap;
+
+	video.pause();
+});
+
+delete_photo_btn.addEventListener("click", function(e) {
+
+	e.preventDefault();
+
+	// Esconder a imagem:
+	image.setAttribute('src', "");
+	image.classList.remove("visible");
+
+	// Desabilitar os botões: excluir e salvar:
+	delete_photo_btn.classList.add("disabled");
+	download_photo_btn.classList.add("disabled");
+
+	video.play();
+});
 
 
 
